2eec0f6692fc41fdf591cf910cffb42ba14c5600
Maria Bazhenova <yankeevsdixie@gmail.com>
2025-09-18 00:00:09 +0300

    first prototype

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { data } from "./data/data";
+function App() {
+ const getRandomNumberBetweenOneAnd114 = () =>
+  Math.floor(Math.random() * 114) + 1;
+
+ const [text, setText] = useState("");
+
+ const getText = () => {
+  const number = getRandomNumberBetweenOneAnd114();
+  const text = data.find((el) => el.id === number);
+  setText(text.text);
+ };
+
+ return (
+  <div className="container">
+   <h1>Система Брайна Ино и Питера Шмидта</h1>
+   <p>
+    Предположим, у вас есть идея, но вы зашли в тупик. Вы не знаете, куда двигаться
+    дальше.
+    Нажмите на кнопку и старайтесь следовать инструкции, которую
+    получите.
+   </p>
+   <button className="btn" onClick={getText}>
+    Жать
+   </button>
+   <div className="instruction">{text}</div>
+  </div>
+ );
+}
+
+export default App;
